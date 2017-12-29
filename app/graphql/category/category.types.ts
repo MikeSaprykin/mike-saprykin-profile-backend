0b29a4203665f1f97222dcd4fa63f8faedd43f80
mikesaprykin <mikesaprykin@gmail.com>
2017-12-29 12:10:17 +0200

    feat(added and fixed query to get technologies of the categories):

--- app/graphql/category/category.types.ts
+++ app/graphql/category/category.types.ts
@@ -1,10 +1,12 @@
 import { GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql';
+import { listString } from '../helpers';
 
 export const categoryType = new GraphQLObjectType({
   name: 'Category',
   fields: () => ({
     _id: { type: GraphQLString },
     title: { type: GraphQLString },
+    technologies: { type: listString },
   }),
 });
 
